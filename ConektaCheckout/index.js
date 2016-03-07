3782b2f3148c74efab5fd4b8449a82b695da7d98
Rogelio Alberto <i@rog.mx>
2016-03-07 00:12:00 -0500

    Add HTML to Component

--- ConektaCheckout/index.js
+++ ConektaCheckout/index.js
@@ -3,9 +3,26 @@ import React from 'react'
 class ReactConektaCheckout extends React.Component {
   render () {
     return (
-      <div>
-        <h1>Conekta Checkout 2</h1>
-      </div>
+      <form className='checkout'>
+         <div className='checkout-header'>
+           <h1 className='checkout-title'>
+             Checkout
+             <span className='checkout-price'>$10</span>
+           </h1>
+         </div>
+         <p>
+           <input type='text' className='checkout-input checkout-name' placeholder='Your name' autoFocus />
+           <input type='text' className='checkout-input checkout-exp' placeholder='MM' />
+           <input type='text' className='checkout-input checkout-exp' placeholder='YY' />
+         </p>
+         <p>
+           <input type='text' className='checkout-input checkout-card' placeholder='4111 1111 1111 1111' />
+           <input type='text' className='checkout-input checkout-cvc' placeholder='CVC' />
+         </p>
+         <p>
+           <input type='submit' value='Purchase' className='checkout-btn' />
+         </p>
+       </form>
     )
   }
 }
