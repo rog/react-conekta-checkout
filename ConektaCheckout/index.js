c434a92954cc4c1d8ddc47adefac0c21c590c781
Rogelio Alberto <i@rog.mx>
2016-04-02 00:12:00 -0500

    Changin styles to BEM

--- ConektaCheckout/index.js
+++ ConektaCheckout/index.js
@@ -10,42 +10,42 @@ class ReactConektaCheckout extends React.Component {
          <div className={styles.checkout__header}>
            <h1 className={styles.checkout__title}>
              Checkout
-             <span className={styles.checkoutPrice}>$42</span>
+             <span className={styles.checkout__price}>$42</span>
            </h1>
          </div>
          <p className={styles.checkout__paragraph}>
            <input
              type='text'
-             className={classNames(styles.checkoutInput, styles.checkoutName)}
+             className={classNames(styles.checkout__input, styles['checkout__input--name'])}
              placeholder='Your name'
              autoFocus />
 
            <input
              type='text'
-             className={classNames(styles.checkoutInput, styles.checkoutExp)}
+             className={classNames(styles.checkout__input, styles['checkout__input--exp'])}
              placeholder='MM' />
 
            <input
              type='text'
-             className={classNames(styles.checkoutInput, styles.checkoutExp)}
+             className={classNames(styles.checkout__input, styles['checkout__input--exp'])}
              placeholder='YY' />
          </p>
          <p className={styles.checkout__paragraph}>
            <input
              type='text'
-             className={classNames(styles.checkoutInput, styles.checkoutCard)}
+             className={classNames(styles.checkout__input, styles['checkout__input--card'])}
              placeholder='4111 1111 1111 1111' />
 
            <input
              type='text'
-             className={classNames(styles.checkoutInput, styles.checkoutCvc)}
+             className={classNames(styles.checkout__input, styles['checkout__input--cvc'])}
              placeholder='CVC' />
          </p>
          <p className={styles.checkout__paragraph}>
            <input
              type='submit'
              value='Purchase'
-             className={styles.checkoutBtn} />
+             className={styles.checkout__button} />
          </p>
        </form>
     )
