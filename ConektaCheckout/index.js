c3230b62f1f4d48b1d42372c0c6bfbe8a1edea1f
Rogelio Alberto <i@rog.mx>
2016-04-03 00:12:00 -0500

    Lint Format to classNames

--- ConektaCheckout/index.js
+++ ConektaCheckout/index.js
@@ -6,6 +6,8 @@ import styles from './styles.css'
 class ReactConektaCheckout extends React.Component {
   render () {
     return (
+      <div>
+      <div className={styles.checkout__overlay}></div>
       <form className={styles.checkout}>
          <div className={styles.checkout__header}>
            <h1 className={styles.checkout__title}>
@@ -16,29 +18,54 @@ class ReactConektaCheckout extends React.Component {
          <p className={styles.checkout__paragraph}>
            <input
              type='text'
-             className={classNames(styles.checkout__input, styles['checkout__input--name'])}
+             className={
+               classNames(
+                 styles.checkout__input,
+                 styles['checkout__input--name']
+               )
+             }
              placeholder='Your name'
              autoFocus />
 
            <input
              type='text'
-             className={classNames(styles.checkout__input, styles['checkout__input--exp'])}
+             className={
+               classNames(
+                 styles.checkout__input,
+                 styles['checkout__input--exp']
+               )
+             }
              placeholder='MM' />
 
            <input
              type='text'
-             className={classNames(styles.checkout__input, styles['checkout__input--exp'])}
+             className={
+               classNames(
+                 styles.checkout__input,
+                 styles['checkout__input--exp']
+               )
+             }
              placeholder='YY' />
          </p>
          <p className={styles.checkout__paragraph}>
            <input
              type='text'
-             className={classNames(styles.checkout__input, styles['checkout__input--card'])}
+             className={
+               classNames(
+                 styles.checkout__input,
+                 styles['checkout__input--card']
+               )
+             }
              placeholder='4111 1111 1111 1111' />
 
            <input
              type='text'
-             className={classNames(styles.checkout__input, styles['checkout__input--cvc'])}
+             className={
+               classNames(
+                 styles.checkout__input,
+                 styles['checkout__input--cvc']
+               )
+             }
              placeholder='CVC' />
          </p>
          <p className={styles.checkout__paragraph}>
@@ -48,6 +75,7 @@ class ReactConektaCheckout extends React.Component {
              className={styles.checkout__button} />
          </p>
        </form>
+       </div>
     )
   }
 }
